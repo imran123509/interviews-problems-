55ca2b55082bdb709a8b928e656a950a8d452952
Imran hassan <72485040+imran123509@users.noreply.github.com>
2022-04-01 11:56:54 +0530

    Add files via upload

---
+++ Dp/Minimum Falling Path Sum.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+// pre req => triangle
+using namespace std;
+//memoize solution
+/*
+
+Time Complexity: O(N*N)
+
+Reason: At max, there will be M*N calls of recursion to solve a new problem,
+
+Space Complexity: O(N) + O(N*M)
+
+Reason: We are using a recursion stack space: O(N), where N is the path length and an external DP Array of size ‘N*M’.
+*/
+int getMaxUtil(int i, int j, int m, vector<vector<int>> &matrix, 
+vector<vector<int> > &dp){
+    
+    // Base Conditions
+    if(j<0 || j>=m)
+        return -1e9;
+    if(i==0)
+        return matrix[0][j];
+    
+    if(dp[i][j]!=-1) return dp[i][j];
+    
+    int up = matrix[i][j] + getMaxUtil(i-1,j,m,matrix,dp);
+    int leftDiagonal = matrix[i][j] + getMaxUtil(i-1,j-1,m,matrix,dp);
+    int rightDiagonal = matrix[i][j] + getMaxUtil(i-1,j+1,m,matrix,dp);
+    
+    return dp[i][j]= max(up,max(leftDiagonal,rightDiagonal));
+    
+}
+//Tabulation solution
+/*
+
+Time Complexity: O(N*M)
+
+Reason: There are two nested loops
+
+Space Complexity: O(N*M)
+
+Reason: We are using an external array of size ‘N*M’. The stack space will be eliminated.
+*/
+int getMaxPathSum1(vector<vector<int> > &matrix){
+    
+    int n = matrix.size();
+    int m = matrix[0].size();
+    
+    vector<vector<int>> dp(n,vector<int>(m,0));
+    
+    // Initializing first row - base condition
+    for(int j=0; j<m; j++){
+        dp[0][j] = matrix[0][j];
+    }
+    
+    for(int i=1; i<n; i++){
+        for(int j=0;j<m;j++){
+            
+            int up = matrix[i][j] + dp[i-1][j];
+            
+            int leftDiagonal= matrix[i][j];
+            if(j-1>=0) leftDiagonal += dp[i-1][j-1];
+            else leftDiagonal += -1e9;
+            
+            int rightDiagonal = matrix[i][j];
+            if(j+1<m) rightDiagonal += dp[i-1][j+1];
+            else rightDiagonal += -1e9;
+            
+            dp[i][j] = max(up, max(leftDiagonal,rightDiagonal));
+            
+        }
+    }
+    
+    int maxi = INT_MIN;
+    
+    for(int j=0; j<m;j++){
+        maxi = max(maxi,dp[n-1][j]);
+    }
+    
+    return maxi;
+}
+
+int getMaxPathSum(vector<vector<int> > &matrix){
+    
+    int n = matrix.size();
+    int m = matrix[0].size();
+    
+    vector<vector<int>> dp(n,vector<int>(m,-1));
+    
+    int maxi = INT_MIN;
+    
+    for(int j=0; j<m;j++){
+        int ans = getMaxUtil(n-1,j,m,matrix,dp);
+        maxi = max(maxi,ans);
+    }
+    
+    return maxi;
+}
+
+int main() {
+
+  vector<vector<int> > matrix{{1,2,10,4},
+                              {100,3,2,1},
+                              {1,1,20,2},
+                              {1,2,2,1}};
+  
+  cout<<getMaxPathSum(matrix);
+}
+
+//https://leetcode.com/problems/minimum-falling-path-sum/description/
