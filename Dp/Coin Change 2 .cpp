3e5227c1c4769ab7e12b909c169d544375b429c9
Imran hassan <72485040+imran123509@users.noreply.github.com>
2022-04-01 18:49:42 +0530

    Add files via upload

---
+++ Dp/Coin Change 2 .cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+//pre req=> target sum
+using namespace std;
+// memoization solution
+/*
+
+Time Complexity: O(N*T)
+
+Reason: There are N*W states therefore at max ‘N*T’ new problems will be solved.
+
+Space Complexity: O(N*T) + O(N)
+
+Reason: We are using a recursion stack space(O(N)) and a 2D array ( O(N*T)).
+*/
+long countWaysToMakeChangeUtil(vector<int>& arr,int ind, int T, vector<vector<long
+>>& dp){
+
+    if(ind == 0){
+        return (T%arr[0]==0);
+    }
+    
+    if(dp[ind][T]!=-1)
+        return dp[ind][T];
+        
+    long notTaken = countWaysToMakeChangeUtil(arr,ind-1,T,dp);
+    
+    long taken = 0;
+    if(arr[ind] <= T)
+        taken = countWaysToMakeChangeUtil(arr,ind,T-arr[ind],dp);
+        
+    return dp[ind][T] = notTaken + taken;
+}
+
+// tabulation solution
+/*
+
+Time Complexity: O(N*T)
+
+Reason: There are two nested loops
+
+Space Complexity: O(N*T)
+
+Reason: We are using an external array of size ‘N*T’. Stack Space is eliminated.
+*/
+long countWaysToMakeChange1(vector<int>& arr, int n, int T){
+    
+    vector<vector<long>> dp(n,vector<long>(T+1,0));
+    
+    
+    //Initializing base condition
+    for(int i=0;i<=T;i++){
+        if(i%arr[0]==0)
+            dp[0][i]=1;
+        // Else condition is automatically fulfilled,
+        // as dp array is initialized to zero
+    }
+    
+    for(int ind=1; ind<n;ind++){
+        for(int target=0;target<=T;target++){
+            long notTaken = dp[ind-1][target];
+            
+            long taken = 0;
+            if(arr[ind]<=target)
+                taken = dp[ind][target-arr[ind]];
+                
+            dp[ind][target] = notTaken + taken;
+        }
+    }
+    
+    return dp[n-1][T];
+}
+long countWaysToMakeChange(vector<int>& arr, int n, int T){
+    
+    vector<vector<long>> dp(n,vector<long>(T+1,-1));
+    return countWaysToMakeChangeUtil(arr,n-1, T, dp);
+}
+
+int main() {
+
+  vector<int> arr ={2, 3 , 5};
+  int target=9;
+  
+  int n =arr.size();
+                                 
+  cout<<"The total number of ways is " <<countWaysToMakeChange(arr,n,target);
+}
+
+//https://cses.fi/problemset/task/1636
