d46e2049e3dcf4921358e3551cc5c6c15b8daa77
Imran hassan <72485040+imran123509@users.noreply.github.com>
2022-04-01 16:29:21 +0530

    Add files via upload

---
+++ Dp/Minimum Coins.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+//pre req=> 0-1 knapsack
+using namespace std;
+// memoization solution
+/*
+
+Time Complexity: O(N*T)
+
+Reason: There are N*T states therefore at max ‘N*T’ new problems will be solved.
+
+Space Complexity: O(N*T) + O(N)
+
+Reason: We are using a recursion stack space(O(N)) and a 2D array ( O(N*T)).
+*/
+
+int minimumElementsUtil(vector<int>& arr, int ind, int T, vector<vector<int>>& dp){
+
+    if(ind == 0){
+        if(T%arr[0] == 0) return T/arr[0];
+        else return 1e9;
+    }
+    
+    if(dp[ind][T]!=-1)
+        return dp[ind][T];
+        
+    int notTaken = 0 + minimumElementsUtil(arr,ind-1,T,dp);
+    
+    int taken = 1e9;
+    if(arr[ind] <= T)
+        taken = 1 + minimumElementsUtil(arr,ind,T-arr[ind],dp);
+        
+    return dp[ind][T] = min(notTaken,taken);
+}
+
+//tabulation solution
+/*
+
+Time Complexity: O(N*T)
+
+Reason: There are two nested loops
+
+Space Complexity: O(N*T)
+
+Reason: We are using an external array of size ‘N*T’. Stack Space is eliminated.
+*/
+int minimumElements1(vector<int>& arr, int T){
+    
+    int n= arr.size();
+    
+    vector<vector<int>> dp(n,vector<int>(T+1,0));
+    
+    for(int i=0; i<=T; i++){
+        if(i%arr[0] == 0)  
+            dp[0][i] = i/arr[0];
+        else dp[0][i] = 1e9;
+    }
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target = 0; target<=T; target++){
+            
+            int notTake = 0 + dp[ind-1][target];
+            int take = 1e9;
+            if(arr[ind]<=target)
+                take = 1 + dp[ind][target - arr[ind]];
+                
+             dp[ind][target] = min(notTake, take);
+        }
+    }
+    
+    int ans = dp[n-1][T];
+    if(ans >=1e9) return -1;
+    return ans;
+}
+
+
+int minimumElements(vector<int>& arr, int T){
+    
+    int n= arr.size();
+    
+    vector<vector<int>> dp(n,vector<int>(T+1,-1));
+    int ans =  minimumElementsUtil(arr, n-1, T, dp);
+    if(ans >= 1e9) return -1;
+    return ans;
+}
+
+int main() {
+
+  vector<int> arr ={1,1,1,1,1};
+  int T=3;
+                                 
+  cout<<"The minimum number of coins required to form the target sum is " 
+  <<minimumElements(arr,T);
+}
