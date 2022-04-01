cf5cd2b7966868dbeb4047c0965bb848f9c3ff57
Imran hassan <72485040+imran123509@users.noreply.github.com>
2022-04-01 10:55:38 +0530

    Add files via upload

---
+++ Dp/Vacation.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+// pre req => House Robbe
+using namespace std;
+
+//memoize solution
+/*
+
+Time Complexity: O(N*4*3)
+
+Reason: There are N*4 states and for every state, we are running a for loop iterating three times.
+
+Space Complexity: O(N) + O(N*4)
+
+Reason: We are using a recursion stack space(O(N)) and a 2D array (again O(N*4)). Therefore total space complexity will be O(N) + O(N) ≈ O(N)
+*/
+int f(int day, int last, vector<vector<int>> &points,vector<vector<int>> &dp) {
+
+  if (dp[day][last] != -1) return dp[day][last];
+
+  if (day == 0) {
+    int maxi = 0;
+    for (int i = 0; i <= 2; i++) {
+      if (i != last)
+        maxi = max(maxi, points[0][i]);
+    }
+    return dp[day][last] = maxi;
+  }
+
+  int maxi = 0;
+  for (int i = 0; i <= 2; i++) {
+    if (i != last) {
+      int activity = points[day][i] + f(day - 1, i, points, dp);
+      maxi = max(maxi, activity);
+    }
+
+  }
+
+  return dp[day][last] = maxi;
+}
+
+//Tabulation solution
+/*
+
+Time Complexity: O(N*4*3)
+
+Reason: There are three nested loops
+
+Space Complexity: O(N*4)
+
+Reason: We are using an external array of size ‘N*4’’.
+*/
+
+int ninjaTraining1(int n, vector < vector < int > > & points) {
+
+  vector < vector < int > > dp(n, vector < int > (4, 0));
+
+  dp[0][0] = max(points[0][1], points[0][2]);
+  dp[0][1] = max(points[0][0], points[0][2]);
+  dp[0][2] = max(points[0][0], points[0][1]);
+  dp[0][3] = max(points[0][0], max(points[0][1], points[0][2]));
+
+  for (int day = 1; day < n; day++) {
+    for (int last = 0; last < 4; last++) {
+      dp[day][last] = 0;
+      for (int task = 0; task <= 2; task++) {
+        if (task != last) {
+          int activity = points[day][task] + dp[day - 1][task];
+          dp[day][last] = max(dp[day][last], activity);
+        }
+      }
+    }
+
+  }
+
+  return dp[n - 1][3];
+}
+
+int ninjaTraining(int n, vector < vector < int > > & points) {
+
+  vector < vector < int > > dp(n, vector < int > (4, -1));
+  return f(n - 1, 3, points, dp);
+}
+
+int main() {
+
+  vector < vector < int > > points = {{10,40,70},
+                                      {20,50,80},
+                                      {30,60,90}};
+
+  int n = points.size();
+  cout << ninjaTraining(n, points);
+}
+
+
+//https://atcoder.jp/contests/dp/tasks/dp_c
