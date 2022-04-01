da818cc82c477e060ad5c36397e616c312d68644
Imran hassan <72485040+imran123509@users.noreply.github.com>
2022-04-01 11:54:49 +0530

    Add files via upload

--- Dp/Minimum path sum in Triangular .cpp
+++ Dp/Minimum path sum in Triangular .cpp
@@ -1,77 +1,110 @@
 #include <bits/stdc++.h>
-// pre req => minimum sum path
+// pre req => triangle
 using namespace std;
 //memoize solution
 /*
 
 Time Complexity: O(N*N)
 
-Reason: At max, there will be (half of, due to triangle) N*N calls of recursion.
+Reason: At max, there will be M*N calls of recursion to solve a new problem,
 
-Space Complexity: O(N) + O(N*N)
+Space Complexity: O(N) + O(N*M)
 
-Reason: We are using a recursion stack space: O((N), where N is the path length and an external DP Array of size ‘N*N’.
+Reason: We are using a recursion stack space: O(N), where N is the path length and an external DP Array of size ‘N*M’.
 */
-int minimumPathSumUtil(int i, int j, vector<vector<int> > &triangle,int n, 
-vector<vector<int> > &dp) {
-  
-  if(i==n-1) return triangle[i][j];
+int getMaxUtil(int i, int j, int m, vector<vector<int>> &matrix, 
+vector<vector<int> > &dp){
     
-  int down = triangle[i][j]+minimumPathSumUtil(i+1,j,triangle,n,dp);
-  int diagonal = triangle[i][j]+minimumPathSumUtil(i+1,j+1,triangle,n,dp);
-  
-  return dp[i][j] = min(down, diagonal);
-  
+    // Base Conditions
+    if(j<0 || j>=m)
+        return -1e9;
+    if(i==0)
+        return matrix[0][j];
+    
+    if(dp[i][j]!=-1) return dp[i][j];
+    
+    int up = matrix[i][j] + getMaxUtil(i-1,j,m,matrix,dp);
+    int leftDiagonal = matrix[i][j] + getMaxUtil(i-1,j-1,m,matrix,dp);
+    int rightDiagonal = matrix[i][j] + getMaxUtil(i-1,j+1,m,matrix,dp);
+    
+    return dp[i][j]= max(up,max(leftDiagonal,rightDiagonal));
+    
 }
 //Tabulation solution
 /*
 
-Time Complexity: O(N*N)
+Time Complexity: O(N*M)
 
 Reason: There are two nested loops
 
-Space Complexity: O(N*N)
+Space Complexity: O(N*M)
 
-Reason: We are using an external array of size ‘N*N’. The stack space will be eliminated.
+Reason: We are using an external array of size ‘N*M’. The stack space will be eliminated.
 */
-int minimumPathSum1(vector<vector<int> > &triangle, int n){
-    vector<vector<int> > dp(n,vector<int>(n,0));
+int getMaxPathSum1(vector<vector<int> > &matrix){
     
-    for(int j=0;j<n;j++){
-        dp[n-1][j] = triangle[n-1][j];
+    int n = matrix.size();
+    int m = matrix[0].size();
+    
+    vector<vector<int>> dp(n,vector<int>(m,0));
+    
+    // Initializing first row - base condition
+    for(int j=0; j<m; j++){
+        dp[0][j] = matrix[0][j];
     }
     
-    for(int i=n-2; i>=0; i--){
-        for(int j=i; j>=0; j--){
+    for(int i=1; i<n; i++){
+        for(int j=0;j<m;j++){
             
-            int down = triangle[i][j]+dp[i+1][j];
-            int diagonal = triangle[i][j]+dp[i+1][j+1];
+            int up = matrix[i][j] + dp[i-1][j];
             
-            dp[i][j] = min(down, diagonal);
+            int leftDiagonal= matrix[i][j];
+            if(j-1>=0) leftDiagonal += dp[i-1][j-1];
+            else leftDiagonal += -1e9;
+            
+            int rightDiagonal = matrix[i][j];
+            if(j+1<m) rightDiagonal += dp[i-1][j+1];
+            else rightDiagonal += -1e9;
+            
+            dp[i][j] = max(up, max(leftDiagonal,rightDiagonal));
+            
         }
     }
     
-    return dp[0][0];
+    int maxi = INT_MIN;
     
+    for(int j=0; j<m;j++){
+        maxi = max(maxi,dp[n-1][j]);
+    }
+    
+    return maxi;
 }
 
-
-int minimumPathSum(vector<vector<int> > &triangle, int n){
-    vector<vector<int> > dp(n,vector<int>(n,-1));
-    return minimumPathSumUtil(0,0,triangle,n,dp);
+int getMaxPathSum(vector<vector<int> > &matrix){
     
+    int n = matrix.size();
+    int m = matrix[0].size();
+    
+    vector<vector<int>> dp(n,vector<int>(m,-1));
+    
+    int maxi = INT_MIN;
+    
+    for(int j=0; j<m;j++){
+        int ans = getMaxUtil(n-1,j,m,matrix,dp);
+        maxi = max(maxi,ans);
+    }
+    
+    return maxi;
 }
 
 int main() {
 
-  vector<vector<int> > triangle{{1},
-                                {2,3},
-                                {3,6,7},
-                                {8,9,6,10}};
-                            
-  int n = triangle.size();
+  vector<vector<int> > matrix{{1,2,10,4},
+                              {100,3,2,1},
+                              {1,1,20,2},
+                              {1,2,2,1}};
   
-  cout<<minimumPathSum(triangle,n);
+  cout<<getMaxPathSum(matrix);
 }
 
-//https://leetcode.com/problems/triangle/description/
+//https://leetcode.com/problems/minimum-falling-path-sum/description/
